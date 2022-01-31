61afe58736718d6530baae638a2c89e997707b63
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-02-01 01:07:12 +0900

    migrationファイルを変更

--- database/migrations/2022_01_31_044321_create_tags_table.php
+++ database/migrations/2022_01_31_044321_create_tags_table.php
@@ -14,8 +14,18 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id', true);
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+
+            // 論理削除を定義→deleted_atを自動生成
+            $table->softDeletes();
+
+            // 外部キー制約
+            // timestampと記載するとレコード登録時、更新時に値が入らないためDB:rawで直接記載
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
