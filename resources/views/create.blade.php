7a3980bdddea7d5aed2e1b84a7ecb8d2b0b7df70
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-02-16 22:52:21 +0900

    画面にタグ一覧を表示するように変更

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,6 +9,13 @@
             <div class="mb-3">
                 <textarea class="form-control" name="content" placeholder="ここにメモを入力" rows="3"></textarea>
             </div>
+        <!-- タグ一覧 -->
+        @foreach ($tags as $tag)
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" name="tags[]" id={{ $tag['id'] }} value={{ $tag['id'] }}>
+                <label class="form-check-label" for={{ $tag['id'] }}>{{ $tag['name'] }}</label>
+            </div>
+        @endforeach
             <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力" />
             <button type="submit" class="btn btn-primary">登録</button>
         </form>
