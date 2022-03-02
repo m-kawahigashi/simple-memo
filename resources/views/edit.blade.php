87cf41612e7bdebf4c65eeb7f049802fa304f339
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-03-02 23:13:44 +0900

    削除ボタン用のアイコンを追加

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -30,7 +30,7 @@
         <form class="card-body" id="delete-form" action={{ route('destroy') }} method="POST">
             @csrf
             <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
-            <button type="submit" class="btn btn-primary" onclick="deleteHandle(event);">削除</button>
+            <i class="fas fa-trash" onclick="deleteHandle(event);"></i>
         </form>
     </div>
 </div>
